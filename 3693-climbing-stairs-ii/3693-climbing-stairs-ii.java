49e7e57aa206d9288035acd2016a9e7213d3f4b5
ASHOK KUMAR P <141118571+Ashok-0167@users.noreply.github.com>
2026-06-22 09:46:48 +0530

    Time: 51 ms (9.31%), Space: 128.6 MB (20.76%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -1,6 +1,5 @@
 class Solution {
     public int climbStairs(int n, int[] costs) {
-        // memo array to store results of subproblems
         int[] memo = new int[n + 1];
         java.util.Arrays.fill(memo, -1);
         
@@ -8,15 +7,12 @@ class Solution {
     }
     
     private int helper(int j, int[] costs, int[] memo) {
-        // Base case: reaching the ground step costs nothing
         if (j == 0) return 0;
         
-        // If already calculated, return the cached result
         if (memo[j] != -1) return memo[j];
         
         int minCost = Integer.MAX_VALUE;
         
-        // Try all 3 possible incoming jumps (from j-1, j-2, j-3)
         for (int jump = 1; jump <= 3; jump++) {
             int i = j - jump;
             if (i >= 0) {
