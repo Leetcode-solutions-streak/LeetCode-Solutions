d51badcbac4308fab28da0cada3d4342d2184ad1
ASHOK KUMAR P <141118571+Ashok-0167@users.noreply.github.com>
2026-09-09 09:11:04 +0530

    Time: 3 ms (19.73%), Space: 19.3 MB (13.61%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countCommas(self, n):
+        if n<=999:
+            return 0
+        
+        tot=0
+        st=1000
+        while st <=n:
+            tot += n-st+1
+            st *=1000
+            
+        return tot
+        
+        
